ea5cfc4b461706e5c5bf0baf65b6cd99bc2e0d16
Журба Дмитрий <dmitriy.zhurba@gazprombank.ru>
2022-08-11 22:19:06 +0300

    lesson_07

--- app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
+++ app/src/main/java/ru/sumin/coroutinestart/MainActivity.kt
@@ -1,9 +1,12 @@
 package ru.sumin.coroutinestart
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import ru.sumin.coroutinestart.databinding.ActivityMainBinding
@@ -20,18 +23,23 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.setOnClickListener {
             binding.progress.isVisible = true
             binding.buttonLoad.isEnabled = false
-            val jobCity = lifecycleScope.launch {
-                val city = loadCity()
-                binding.tvLocation.text = city
+            val deferredCity: Deferred<String> = lifecycleScope.async {
+                loadCity()
             }
-            val jobTemp = lifecycleScope.launch {
-                val temp = loadTemperature()
-                binding.tvTemperature.text = temp.toString()
+            val deferredTemp: Deferred<Int> = lifecycleScope.async {
+                loadTemperature()
             }
 
             lifecycleScope.launch {
-                jobCity.join()
-                jobTemp.join()
+                val city = deferredCity.await()
+                val temp = deferredTemp.await()
+                binding.tvLocation.text = city
+                binding.tvTemperature.text = temp.toString()
+                Toast.makeText(
+                    this@MainActivity,
+                    "City: $city, temp: $temp",
+                    Toast.LENGTH_SHORT
+                ).show()
                 binding.progress.isVisible = false
                 binding.buttonLoad.isEnabled = true
             }
